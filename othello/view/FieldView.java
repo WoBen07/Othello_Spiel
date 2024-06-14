d39afe153b225dbd6a4ea5410203026837656593
highestPACE <pschweizer0711@gmail.com>
2024-06-14 21:01:36 +0200

    Visualisierung der Occupation vorläufig mit Hintergrundfarbe implementiert

--- othello/view/FieldView.java
+++ othello/view/FieldView.java
@@ -1,5 +1,6 @@
 package othello.view;
 
+import java.awt.Color;
 import java.awt.Dimension;
 
 import javax.swing.JButton;
@@ -8,14 +9,22 @@ import othello.Occupation;
 
 @SuppressWarnings("serial")
 public class FieldView extends JButton {
+    
+    private static Color color = new Color(0, 128, 0);
 
     public FieldView() {
+	setBackground(color);
+	
 	setPreferredSize(new Dimension(100, 100));
     }
 
     public void setOccupationView(Occupation occupation) {
-	if (occupation == Occupation.BLACK) {
-	    
+	if (occupation == Occupation.NONE) {
+	    setBackground(color);
+	} else if (occupation == Occupation.BLACK) {
+	    setBackground(Color.BLACK);
+	} else {
+	    setBackground(Color.WHITE);
 	}
     }
 }
