c17a09508ea356682ce57b9c96ca0190ea7bbcb7
highestPACE <pschweizer0711@gmail.com>
2024-06-14 19:53:36 +0200

    Spielsteine versucht durch Image-File zu implementieren

--- othello/view/FieldView.java
+++ othello/view/FieldView.java
@@ -2,6 +2,7 @@ package othello.view;
 
 import java.awt.Dimension;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 @SuppressWarnings("serial")
@@ -9,5 +10,6 @@ public class FieldView extends JButton {
 
     public FieldView() {
 	setPreferredSize(new Dimension(100, 100));
+	setIcon(new ImageIcon("Semesteraufgabe/Othello_Spiel/othello/view/blackCircle.png"));
     }
 }
