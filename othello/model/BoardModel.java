3af8ef0c07efbab429b00dcc35e1eada860d8b51
highestPACE <pschweizer0711@gmail.com>
2024-06-21 10:24:40 +0200

    BoardModel-Konstruktor hat jetzt Occupations als Eingabeparameter und erzeugt FieldModels entsprechend

--- othello/model/BoardModel.java
+++ othello/model/BoardModel.java
@@ -5,18 +5,51 @@ import othello.Occupation;
 public class BoardModel {
 
     private final FieldModel[][] fields = new FieldModel[8][8];
+    private Occupation[][] fieldOccupations;
     private boolean isBlacksTurn = true;
 
-    public BoardModel() {
+    public BoardModel(Occupation[][] fieldOccupations) {
+	setFieldOccupations(fieldOccupations);
+	setFields();
+    }
+
+    private static boolean checkFieldOccupations(
+	    Occupation[][] fieldOccupations) {
+
+	if (fieldOccupations.length != 8) {
+	    return false;
+	}
 	for (int i = 0; i < 8; ++i) {
-	    for (int j = 0; j < 8; ++j) {
-		fields[i][j] = new FieldModel();
+	    if (fieldOccupations[i].length != 8) {
+		return false;
 	    }
 	}
+	return true;
     }
 
     public FieldModel[][] getFields() {
 	return fields;
+    }
+
+    private void setFields() {
+	for (int i = 0; i < 8; ++i) {
+	    for (int j = 0; j < 8; ++j) {
+		getFields()[i][j] = new FieldModel(getFieldOccupations()[i][j]);
+	    }
+	}
+    }
+
+    public Occupation[][] getFieldOccupations() {
+	return fieldOccupations;
+    }
+
+    public void setFieldOccupations(Occupation[][] fieldOccupations) {
+	if (checkFieldOccupations(fieldOccupations)) {
+	    this.fieldOccupations = fieldOccupations;
+	} else {
+	    // TODO eigene Exception implementieren
+	    throw new IllegalArgumentException("64 occupation-details needed");
+	}
     }
 
     public boolean isBlacksTurn() {
--- othello/model/FieldModel.java
+++ othello/model/FieldModel.java
@@ -6,10 +6,14 @@ public class FieldModel {
 
     private Occupation occupation = Occupation.NONE;
 
+    public FieldModel(Occupation occupation) {
+	setOccupation(occupation);
+    }
+
     public Occupation getOccupation() {
 	return occupation;
     }
-    
+
     public void setOccupation(Occupation occupation) {
 	this.occupation = occupation;
     }
