dbf72eca631ce436818c01181ae8988741615c92
highestPACE <pschweizer0711@gmail.com>
2024-06-30 16:58:12 +0200

    comments in checkMove() für Übersichtlichkeit entfernt

--- othello/model/BoardModel.java
+++ othello/model/BoardModel.java
@@ -10,12 +10,9 @@ import othello.Piece;
 @SuppressWarnings("serial")
 public class BoardModel implements Serializable {
 
-    private static int ROWS = 8;
-    private static int COLS = 8;
-
     private PropertyChangeSupport changes = new PropertyChangeSupport(this);
 
-    private final FieldModel[][] fields = new FieldModel[ROWS][COLS];
+    private final FieldModel[][] fields = new FieldModel[8][8];
     private Piece[][] pieceFormation;
     private boolean darksTurn = true;
     private boolean[][] legalMoves;
@@ -114,65 +111,27 @@ public class BoardModel implements Serializable {
 	}
     }
 
-    public boolean wasPassPlayed() {
-	return passPlayed;
-    }
-
-    public void setPassPlayed(boolean passPlayed) {
-	this.passPlayed = passPlayed;
-    }
-
     private boolean checkMove(int xNewPiece, int yNewPiece,
 	    boolean flipPieces) {
 
 	boolean atLeastOnePieceFlipped = false;
-
-	int[][] directions = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 }, // vertikal
-									 // und
-									 // horizontal
-		{ -1, -1 }, { 1, 1 }, { -1, 1 }, { 1, -1 } // diagonal
-	};
-
-	// geht durch alle mglichen richtungen durch und checkt falls etwas
-	// umgedreht werden muss
+	int[][] directions = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 },
+		{ -1, -1 }, { 1, 1 }, { -1, 1 }, { 1, -1 } };
 	for (int[] direction : directions) {
 	    if (xNewPiece + direction[0] >= 0 && xNewPiece + direction[0] < 8
 		    && yNewPiece + direction[1] >= 0
 		    && yNewPiece + direction[1] < 8) {
 
-		boolean opponentPieceFound = false; // true fals min. ein Stein
-						    // des
-						    // Gegners in die jeweilige
-						    // Richtung gefunden wird
-						    // (ohne
-						    // Leere felder dazwischen)
+		boolean opponentPieceFound = false;
 		int x = xNewPiece + direction[0];
 		int y = yNewPiece + direction[1];
-
-		// OpponentPieceFound muss nur einmal true gesetzt werden, wenn
-		// min. ein generischer Stein in die Richtung gefunden wird,
-		// um anschliesend wenn ein eigener Stein gefunden wird, zu
-		// bestaetigen dass auch min.
-		// ein gegnerischer Stein dazwischen liegt
 		if (getPieceFormation()[x][y] == (isDarksTurn() ? Piece.LIGHT
 			: Piece.DARK)) {
+
 		    opponentPieceFound = true;
-
-		    // wenn der neue Platz einen eigenen Stein hat und min. ein
-		    // gegnerischer Stein dazwischen liegt,
-		    // dann drehe alle diese gegnerischen Steine um
 		}
-
-		// geht in die jeweilige Richtung bis ein eigener Stein gefunden
-		// wird
 		if (opponentPieceFound) {
-		    while (x >= 0 && x < 8 && y >= 0 && y < 8) { // damit nicht
-								 // außerhalb
-								 // des
-								 // Spielfeldes
-								 // gecheckt
-								 // wird
-
+		    while (x >= 0 && x < 8 && y >= 0 && y < 8) {
 			if (getPieceFormation()[x][y] == Piece.NONE) {
 			    break;
 			}
@@ -181,10 +140,6 @@ public class BoardModel implements Serializable {
 
 			    int flipX = xNewPiece + direction[0];
 			    int flipY = yNewPiece + direction[1];
-
-			    // flip sollange die Steine bis die Koordinaten des
-			    // eigenen
-			    // Steines erreicht werden
 			    if (flipPieces) {
 				while (flipX != x || flipY != y) {
 				    updatePieceFormation(flipX, flipY,
@@ -197,16 +152,12 @@ public class BoardModel implements Serializable {
 			    atLeastOnePieceFlipped = true;
 			    break;
 			}
-
-			// geht in die jeweilige richtung weiter
 			x += direction[0];
 			y += direction[1];
 		    }
 		}
 	    }
-
 	}
-
 	return atLeastOnePieceFlipped;
     }
 
@@ -240,6 +191,14 @@ public class BoardModel implements Serializable {
 
     public boolean hasLegalMoves() {
 	return hasLegalMove;
+    }
+
+    public boolean wasPassPlayed() {
+	return passPlayed;
+    }
+
+    public void setPassPlayed(boolean passPlayed) {
+	this.passPlayed = passPlayed;
     }
 
     public void flipPieces(int xNewPiece, int yNewPiece) {
